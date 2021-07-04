8249bf718312f9f615447d23a4b8d2283b762862
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2021-07-04 20:49:21 +0700

    Add Filter

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 const chatButton = document.querySelector('.chatbox__button');
 const chatContent = document.querySelector('.chatbox__support');
 const icons = {
-    isClicked: '<img src="../assets/chatbox/question_answer_black_24dp.svg" />',
-    isNotClicked: '<img src="../assets/chatbox/question_answer_black_24dp.svg" />'
+    isClicked: '<img src="../assets/chatbox/question_answer_black_24dp.svg" width="30" />',
+    isNotClicked: '<img src="../assets/chatbox/question_answer_black_24dp.svg" width="30" />'
 }
 const chatbox = new InteractiveChatbox(chatButton, chatContent, icons);
 chatbox.display();
